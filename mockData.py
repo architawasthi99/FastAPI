e11299805e8e100f55058bc5ba089f2ff3b6ed8c
Archit Awasthi <167566400+architawasthi99@users.noreply.github.com>
2026-03-02 11:46:16 +0530

    Add files via upload

---
+++ mockData.py
@@ -0,0 +1,14 @@
+product = [
+    {
+        "id":1,
+        "title":"mobile phone",
+        "price":10000,
+        "count":10
+    },
+    {
+        "id":2,
+        "title":"mackbook",
+        "price":10000,
+        "count":5
+    }
+]
