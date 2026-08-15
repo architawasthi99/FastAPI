649af473fe7b3fc3d1eb11d307300be0d1a249dd
Archit Awasthi <167566400+architawasthi99@users.noreply.github.com>
2026-08-15 10:40:18 +0530

    Create FastAPI application with Pydantic model

---
+++ 02_get_api_pydantic.py
@@ -0,0 +1,15 @@
+#GET API 
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+
+app=FastAPI()
+
+class Item(BaseModel):
+    name:str
+    price:int
+    tax:Optional[float]=None
+
+@app.get("/")     
+async def read_root():
+    return{"message":"Hello Coders"}
